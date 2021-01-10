acea8e0289558401e7c47d7367ae1706ff4a5156
antemedia-01 <oneethiopiagigi03@gmail.com>
2021-01-10 12:27:10 -0700

    Update index.js

--- Test/index.js
+++ Test/index.js
@@ -1,5 +1,5 @@
-let time = require('./myModule');
 let http = require('http');
+let time = require('./myModule');
 http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('the date and time is currently:' + time.setTime());
