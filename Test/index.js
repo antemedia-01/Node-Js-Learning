c38d60abcdd9ba0ccb67ebf49a45159b423c72db
antemedia-01 <oneethiopiagigi03@gmail.com>
2021-01-10 12:42:41 -0700

    Update index.js

--- Test/index.js
+++ Test/index.js
@@ -2,7 +2,7 @@ let http = require('http');
 let time = require('./myModule');
 http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('Anteneh you did it bro:' + time.setTime());
+    res.write('Anteneh you did it bro: ' + time.setTime());
     res.end();
 }).listen(8080);
 // console.log(time.setTime());
